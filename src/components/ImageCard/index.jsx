a91286e84ca639b3b78c78851afefd40726afe81
Skeby <103804946+Skeby@users.noreply.github.com>
2023-10-03 12:20:43 +0100

    chore: set time delay on image load

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -5,9 +5,11 @@ import { CSS } from "@dnd-kit/utilities";
 import Loader from "../Loader";
 import ImageTag from "../ImageTag";
 import "./style.css";
+import { useEffect } from "react";
 
 const ImageCard = ({ image }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: image.id });
 
@@ -20,6 +22,19 @@ const ImageCard = ({ image }) => {
   const handleImageLoad = () => {
     setIsLoading(false);
   };
+
+  useEffect(() => {
+    const delay = 1000; // Delay in milliseconds
+
+    // Simulate the image loading process
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+      setIsLoading(false);
+    }, delay);
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div
@@ -36,12 +51,15 @@ const ImageCard = ({ image }) => {
           <Loader />
         </div>
       )}
-      <img
-        src={image.url}
-        alt={image.tag}
-        style={{ display: isLoading ? "none" : "block" }}
-        onLoad={handleImageLoad}
-      ></img>
+      {isLoaded && (
+        <img
+          src={image.url}
+          alt={image.tag}
+          style={{ display: isLoading ? "none" : "block" }}
+          onLoad={handleImageLoad}
+        ></img>
+      )}
+
       <div
         className="image-tag-container"
         style={{ display: isLoading ? "none" : "flex" }}
